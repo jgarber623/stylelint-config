f0f0711ea35250b36342bf75b597794b8cc150a9
Jason Garber <jason@sixtwothree.org>
2023-07-08 23:04:38 -0400

    Add test suite

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,14 @@
-module.exports = require('@jgarber/eslint-config');
+const config = require('@jgarber/eslint-config');
+const globals = require('globals');
+
+module.exports = [
+  ...config,
+  {
+    files: ['spec/**/*[sS]pec.?(m)js'],
+    languageOptions: {
+      globals: {
+        ...globals.jasmine
+      }
+    }
+  }
+];
