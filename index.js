e3f9bd142eafe290ff99b4b9060a7f27a0762f8c
Jason Garber <jason@sixtwothree.org>
2023-07-09 14:17:02 -0400

    Add and test stylelint-stylistic plugin
    This plugin re-integrates the "style" rules that Stylelint deprecated in
    version 15.0.0. The official recommendation is to use Prettier, but…
    I'd rather not add Yet Another Tool to all of my projects _and_ I find
    the plugin's defaults to be well-considered.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 module.exports = {
-  extends: 'stylelint-config-standard',
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-stylistic/config'
+  ],
   plugins: ['stylelint-order'],
   rules: {
-    'order/properties-alphabetical-order': true
+    'order/properties-alphabetical-order': true,
+    'stylistic/linebreaks': 'unix',
+    'stylistic/unicode-bom': 'never'
   }
 };
--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -3,20 +3,36 @@ const stylelint = require('stylelint');
 const config = require('../index');
 
 it('exports an Object', () => {
-  expect(config).toEqual(jasmine.objectContaining({ extends: 'stylelint-config-standard' }));
+  expect(config).toEqual(jasmine.objectContaining({
+    extends: ['stylelint-config-standard', 'stylelint-stylistic/config'],
+    plugins: ['stylelint-order']
+  }));
 });
 
 it('loads the config and validates correct syntax', async () => {
-  const results = await stylelint.lint({ code: 'html { margin: 0 }', config });
+  const code = 'html { margin: 0; }\n';
+  const { errored } = await stylelint.lint({ code, config });
 
-  expect(results.errored).toBe(false);
+  expect(errored).toBe(false);
 });
 
 it('loads the config and invalidates incorrect syntax', async () => {
-  const results = await stylelint.lint({ code: 'html { padding: 0; margin: 0 }', config });
+  const code = 'html {padding: 0\r\n; margin: 0 }';
+  const { errored, results } = await stylelint.lint({ code, config });
 
-  const rules = results.results[0].warnings.map(({ rule }) => rule);
+  expect(errored).toBe(true);
 
-  expect(results.errored).toBe(true);
-  expect(rules).toEqual(jasmine.arrayContaining(['declaration-block-single-line-max-declarations', 'order/properties-alphabetical-order']));
+  expect(results[0].warnings.map(({ rule }) => rule))
+    .toEqual(jasmine.arrayContaining(
+      [
+        'order/properties-alphabetical-order',
+        'stylistic/block-closing-brace-newline-before',
+        'stylistic/block-opening-brace-newline-after',
+        'stylistic/declaration-block-semicolon-newline-after',
+        'stylistic/declaration-block-semicolon-space-before',
+        'stylistic/declaration-block-trailing-semicolon',
+        'stylistic/linebreaks',
+        'stylistic/no-missing-end-of-source-newline'
+      ]
+    ));
 });
