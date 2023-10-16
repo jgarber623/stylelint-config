78bcd7cd8ba172a09dccca6e9657e0ad8f7a180c
Jason Garber <jason@sixtwothree.org>
2023-10-15 20:57:10 -0400

    Use double-slash comment style (#14)

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ module.exports = {
   ],
   plugins: ['stylelint-order'],
   rules: {
-    /*
-     * Require an empty line before at-rules.
-     *
-     * @see {@linke https://stylelint.io/user-guide/rules/at-rule-empty-line-before/}
-     */
+    // Require an empty line before at-rules.
+    //
+    // https://stylelint.io/user-guide/rules/at-rule-empty-line-before/
     'at-rule-empty-line-before': [
       'always',
       {
@@ -18,25 +16,19 @@ module.exports = {
       }
     ],
 
-    /*
-     * Specify the alphabetical order of properties within declaration blocks.
-     *
-     * @see {@link https://github.com/hudochenkov/stylelint-order/blob/master/rules/properties-alphabetical-order/README.md}
-     */
+    // Specify the alphabetical order of properties within declaration blocks.
+    //
+    // https://github.com/hudochenkov/stylelint-order/blob/master/rules/properties-alphabetical-order/README.md
     'order/properties-alphabetical-order': true,
 
-    /*
-     * Specify Unix or Windows linebreaks.
-     *
-     * @see {@link https://github.com/elirasza/stylelint-stylistic/tree/main/lib/rules/linebreaks}
-     */
+    // Specify Unix or Windows linebreaks.
+    //
+    // https://github.com/elirasza/stylelint-stylistic/tree/main/lib/rules/linebreaks
     'stylistic/linebreaks': 'unix',
 
-    /*
-     * Disallow the Unicode Byte Order Mark.
-     *
-     * @see {@link https://github.com/elirasza/stylelint-stylistic/tree/main/lib/rules/unicode-bom}
-     */
+    // Disallow the Unicode Byte Order Mark.
+    //
+    // https://github.com/elirasza/stylelint-stylistic/tree/main/lib/rules/unicode-bom
     'stylistic/unicode-bom': 'never'
   }
 };
